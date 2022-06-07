f88571047b8e4141553efc2536997e5bd3a5f0ac
Ekweaga <hikay133@gmail.com>
2022-06-07 09:14:01 +0100

    AboutUs, contactUs pages added with a home page modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { MeetusComponent } from './meetus/meetus.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent
+},
+{
+  path:'about',
+  component:AboutusComponent
+},
+{
+  path:'meetus',
+  component:MeetusComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { MeetusComponent } from './meetus/meetus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    HomeComponent,
+    MeetusComponent
   ],
   imports: [
     BrowserModule,
